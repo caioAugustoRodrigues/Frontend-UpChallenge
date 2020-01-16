9a58a953e543c1eaffbc5bdf3eea3e83c8f9f0cd
caioAugustoRodrigues <caio.turmaxii@gmail.com>
2020-01-15 22:19:42 -0300

    Updates

---
+++ precache-manifest.dd8cf001848cf18df81b80f40ffa1088.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8137d9e67b596e8cc79a55f600525216",
+    "url": "/Frontend-UpChallenge/index.html"
+  },
+  {
+    "revision": "21524b902d270729ebe0",
+    "url": "/Frontend-UpChallenge/static/css/main.e3749838.chunk.css"
+  },
+  {
+    "revision": "faaf877eebab05062e8f",
+    "url": "/Frontend-UpChallenge/static/js/2.4d53612d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Frontend-UpChallenge/static/js/2.4d53612d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21524b902d270729ebe0",
+    "url": "/Frontend-UpChallenge/static/js/main.c08029ce.chunk.js"
+  },
+  {
+    "revision": "66515ac629847c7f7f6e",
+    "url": "/Frontend-UpChallenge/static/js/runtime-main.8fc047c6.js"
+  },
+  {
+    "revision": "46b0d95b9a2332b222ff362a7df5f7b6",
+    "url": "/Frontend-UpChallenge/static/media/b-b.46b0d95b.png"
+  },
+  {
+    "revision": "5d673c2b0025295c0a9366934e26ac8c",
+    "url": "/Frontend-UpChallenge/static/media/b-lq.5d673c2b.png"
+  },
+  {
+    "revision": "941cc2372ad77a1849b9fac99e962ce8",
+    "url": "/Frontend-UpChallenge/static/media/b-rq.941cc237.png"
+  },
+  {
+    "revision": "c693864dd8cf308e4dd8a3b363b0b759",
+    "url": "/Frontend-UpChallenge/static/media/bg.c693864d.jpg"
+  },
+  {
+    "revision": "d0d2cd1752d215c4df68e76e976ec629",
+    "url": "/Frontend-UpChallenge/static/media/bg.d0d2cd17.gif"
+  }
+]);
